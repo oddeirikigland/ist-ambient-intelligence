4fecfddcae14b39e21d2a434eaf7f84506ff5dde
Odd Eirik Igland <oddeirik.igland@gmail.com>
2020-03-06 17:57:27 +0000

    saves and recognice faces

--- src/components/faceRecognition/ObjectDetectionSketch.js
+++ src/components/faceRecognition/ObjectDetectionSketch.js
@@ -1,12 +1,54 @@
-//import * as p5 from "p5";
-// import "p5/lib/addons/p5.dom";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import * as faceapi from "face-api.js";
+
+import oddi from "./faces/oddi";
+import ingeboss from "./faces/ingeboss";
+import lysbrytern from "./faces/lysbrytern";
+const persons = [oddi, ingeboss, lysbrytern];
 
 const MODEL_URL = "/models";
 const HEIGHT = 200;
 const WIDTH = HEIGHT * 1.7778;
 const TEXT_SIZE = 15;
+const SHOW_DETECTION_DATA = false;
+const cameraOperations = {
+  REGISTER_PERSON: "register_person",
+  RECOGNICE_PERSON: "recognice_person"
+};
+let registeredPictures = 0;
+let personDescriptors = [];
+
+// Options for program
+const PICTURES_TO_TAKE = 10;
+let cameraOperation = cameraOperations.RECOGNICE_PERSON;
+let nameToRegister = "new_name";
+
+const loadFaceDescriptors = () => {
+  const faceDescriptor = name => faceapi.LabeledFaceDescriptors.fromJSON(name);
+  return persons.map(person => faceDescriptor(person));
+};
+
+const handleSaveToPC = (jsonData, filename) => {
+  const fileData = JSON.stringify(jsonData);
+  const blob = new Blob([fileData], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.download = `${filename}.json`;
+  link.href = url;
+  link.click();
+};
+
+const get_expression_value = raw_expressions => {
+  const copiedExpression = raw_expressions;
+  const expressions = Object.keys(copiedExpression).map(key => {
+    const value = copiedExpression[key];
+    return value;
+  });
+  const max = Math.max(...expressions);
+  return Object.keys(copiedExpression).filter(key => {
+    return copiedExpression[key] === max;
+  })[0];
+};
 
 export default function sketch(p) {
   let capture = null;
@@ -27,6 +69,8 @@ export default function sketch(p) {
     await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
     await faceapi.loadAgeGenderModel(MODEL_URL);
     await faceapi.loadFaceExpressionModel(MODEL_URL);
+    await faceapi.loadFaceLandmarkModel(MODEL_URL);
+    await faceapi.loadFaceRecognitionModel(MODEL_URL);
 
     p.createCanvas(WIDTH, HEIGHT);
     const constraints = {
@@ -109,19 +153,8 @@ export default function sketch(p) {
         const ageTextWidth = p.textWidth(agetext);
         p.text(agetext, textX - ageTextWidth - 10, textY - 30);
 
-        const copiedExpression = drawing.expressions;
-        const expressions = Object.keys(copiedExpression).map(key => {
-          const value = copiedExpression[key];
-          return value;
-        });
-
-        const max = Math.max(...expressions);
-
-        const expression_value = Object.keys(copiedExpression).filter(key => {
-          return copiedExpression[key] === max;
-        })[0];
-
-        const expressiontext = "Mood: " + expression_value;
+        const expressiontext =
+          "Mood: " + get_expression_value(drawing.expressions);
         const expressionWidth = p.textWidth(expressiontext);
         p.text(expressiontext, textX - expressionWidth - 10, textY - 10);
 
@@ -135,15 +168,52 @@ export default function sketch(p) {
         );
       }
     });
+
     faceapi
       .detectAllFaces(capture.id())
-      .withAgeAndGender()
+      .withFaceLandmarks()
       .withFaceExpressions()
+      .withAgeAndGender()
+      .withFaceDescriptors()
       .then(data => {
-        showFaceDetectionData(data);
+        if (SHOW_DETECTION_DATA) showFaceDetectionData(data);
+        console.log(data.length);
+        if (data.length) {
+          const person = data[0];
+          if (
+            cameraOperation === cameraOperations.REGISTER_PERSON &&
+            registeredPictures < PICTURES_TO_TAKE
+          ) {
+            registeredPictures++;
+            personDescriptors.push(person.descriptor);
+          } else if (
+            cameraOperation === cameraOperations.REGISTER_PERSON &&
+            registeredPictures === PICTURES_TO_TAKE
+          ) {
+            registeredPictures++;
+            const labelDescriptor = [
+              new faceapi.LabeledFaceDescriptors(
+                nameToRegister,
+                personDescriptors
+              )
+            ];
+            const labelJson = labelDescriptor[0].toJSON();
+            handleSaveToPC(labelJson, nameToRegister);
+            console.log(`${nameToRegister} saved to store`);
+            cameraOperation = cameraOperations.RECOGNICE_PERSON;
+          } else if (cameraOperation === cameraOperations.RECOGNICE_PERSON) {
+            const faceMatcher = new faceapi.FaceMatcher(loadFaceDescriptors());
+            const bestMatch = faceMatcher.findBestMatch(person.descriptor);
+            console.log(
+              `${bestMatch.toString()}, ${person.gender}, ${person.age.toFixed(
+                0
+              )}, ${get_expression_value(person.expressions)}`
+            );
+          }
+        }
       });
 
-    if (capture.loadedmetadata) {
+    if (SHOW_DETECTION_DATA && capture.loadedmetadata) {
       if (cocossdModel) {
         cocossdModel
           .detect(document.getElementById("video_element"))
