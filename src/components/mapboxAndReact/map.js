75a5b00484b7a06937c95a07e4d15741e458a1ca
Ingeborg Sollid <ingsollid@gmail.com>
2020-03-18 10:23:25 +0100

    Adjust map size and tokenfix

--- src/components/mapboxAndReact/map.js
+++ src/components/mapboxAndReact/map.js
@@ -5,9 +5,10 @@ import { nullableTypeAnnotation } from "@babel/types";
 import "./map.css";
 
 const styles = {
-  height: "200px",
-  width: "250px",
-  position: "absolute"
+  height: "216px",
+  width: "300px",
+  position: "absolute",
+  display: "block"
 };
 
 function Map() {
@@ -15,12 +16,11 @@ function Map() {
   const mapContainer = useRef(null);
 
   useEffect(() => {
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoiaXNvbGxpZCIsImEiOiJjazd0MGc5c2cwamtoM2xuMWQwYzhpZ2x2In0.wwIcASVukG2sAQD9YUyIdg";
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
     const initializeMap = ({ setMap, mapContainer }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
-        style: "mapbox://styles/mapbox/streets-v11",
+        style: "mapbox://styles/isollid/ck7tegpyv1b8h1iob8z6ipnom",
         center: [-9.137, 38.724],
         zoom: 11.5
       });
