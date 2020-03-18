88dc11bd1a3b2bb155d76bb5d1907cb225b67ee8
Odd Eirik Igland <oddeirik.igland@gmail.com>
2020-03-18 21:39:21 +0100

    file rename

--- src/components/faceRecognition/faceRec.js
+++ src/components/faceRecognition/faceRec.js
@@ -1,10 +1,10 @@
 import React from "react";
 import P5Wrapper from "react-p5-wrapper";
-import objectDetectionSketch from "./ObjectDetectionSketch";
+import faceDetectionSketch from "./faceDetectionSketch";
 import registerFaceSketch from "./registerFaceSketch"
 
 function FaceRec() {
-  return <P5Wrapper sketch={objectDetectionSketch} />;
+  return <P5Wrapper sketch={faceDetectionSketch} />;
 }
 
 export default FaceRec;
