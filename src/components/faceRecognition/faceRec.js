1e38a78accdc5b0dce51b4512b908f745cdca9a3
Ingeborg Sollid <ingsollid@gmail.com>
2020-03-29 11:41:36 +0200

    Adapt components to new style

--- src/components/faceRecognition/faceRec.js
+++ src/components/faceRecognition/faceRec.js
@@ -1,16 +1,19 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import P5Wrapper from "react-p5-wrapper";
-
 import faceDetectionSketch from "./faceDetectionSketch";
 import { PersonContext } from "../../PersonContextProvider";
+
+import "./faceRec.css";
 
 function FaceRec() {
   const context = useContext(PersonContext);
   return (
-    <div>
+    <div className="faceRec-wrapper">
       <P5Wrapper sketch={faceDetectionSketch} setPerson={context.setPerson} />
-      <Link to="/register">Register your face</Link>
+      <Link className="register-button" to="/register">
+        Register your face
+      </Link>
     </div>
   );
 }
