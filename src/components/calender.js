ba97f5ad56323240d7e020c948f641defebd53f6
Ingeborg Sollid <ingsollid@gmail.com>
2020-05-04 14:07:30 +0200

    Change to google calender

--- src/components/calender.js
+++ src/components/calender.js
@@ -1,103 +1,20 @@
 import React from "react";
-import "./calender.css";
+import Iframe from "react-iframe";
 
-function Calender() {
-  const today = new Date();
-
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric"
-  };
-
-  const activities = [
-    {
-      title: "Odd Eirik Morning Run",
-      date: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 7)
-    },
-
-    {
-      title: "Lunch with neighbors",
-      date: new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate(),
-        12,
-        0
-      )
-    },
-    {
-      title: "Family Dinner",
-      date: new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate(),
-        18,
-        0
-      )
-    },
-    {
-      title: "Anna book circle",
-      date: new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate(),
-        17,
-        0
-      )
-    },
-    {
-      title: "Odd Eirik tennis lesson",
-      date: new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate(),
-        20,
-        0
-      )
-    },
-    {
-      title: "Ingeborg Surf lesson",
-      date: new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate(),
-        21,
-        0
-      )
-    }
-  ];
-  const three_scheduleactivities = activities
-    .sort(() => Math.random() - Math.random())
-    .slice(0, 3)
-    .sort((a, b) => (a.date.getHours() > b.date.getHours() ? 1 : -1))
-
-    .map(act => {
-      return (
-        <div
-          className={
-            Date.now() > act.date
-              ? "schedule-activity-disable"
-              : "schedule-activity"
-          }
-        >
-          <p className="schedule-activity-text">
-            {act.date.getHours()}:00-{act.date.getHours() + 1}:00
-          </p>
-          <p className="schedule-activity-text">{act.title}</p>
-        </div>
-      );
-    });
-
+const Calender = (props) => {
   return (
-    <div className="schedule">
-      <h3 className="box-header">
-        Today: {today.toDateString("en-US", options)}
-      </h3>
-      <div>{three_scheduleactivities}</div>
-    </div>
+    <Iframe
+      url="https://calendar.google.com/calendar/embed?height=200&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FOslo&amp;src=MWUxN3FyOG5xZDZhOXVqOWQ2MjAwMGw4Z3NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000&amp;mode=AGENDA&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showDate=0&amp;showNav=0&amp;showTitle=0&amp;hl=en&amp;showTz=0"
+      style="border-width:0"
+      width="300px"
+      height="200px"
+      id="myId"
+      className="myClassname"
+      display="initial"
+      position="relative"
+      allowFullScreen
+    />
   );
-}
+};
 
 export default Calender;
