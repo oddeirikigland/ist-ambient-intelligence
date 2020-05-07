72c397d00ed4f765106b5cce35291a12d4f40787
Odd Eirik Igland <oddeirik.igland@gmail.com>
2020-05-07 19:06:12 +0200

    current weather icon

--- src/components/weather.js
+++ src/components/weather.js
@@ -24,6 +24,20 @@ class Weather extends React.Component {
       .catch((error) => console.log(error));
   }
 
+  renderWeatherIcon() {
+    console.log(this.state.weather.weather[0].icon);
+    return this.state.weather && this.state.weather.weather[0] ? (
+      <img
+        alt=""
+        width="100"
+        height="100"
+        src={`https://openweathermap.org/img/w/${this.state.weather.weather[0].icon}.png`}
+      />
+    ) : (
+      <p></p>
+    );
+  }
+
   render() {
     if (!this.state.locationFound && this.props.coords) {
       this.getWeather(this.props.coords.latitude, this.props.coords.longitude);
@@ -33,10 +47,27 @@ class Weather extends React.Component {
     return weather ? (
       <div className="weather">
         <h3 className="comp-header">Current weather</h3>
-        <p>Location: {weather.name}</p>
-        <p>Weather: {weather.weather[0].description}</p>
-        <p>Temperature: {weather.main.temp}</p>
-        <p>Wind Speed: {weather.wind.speed}</p>
+        <div
+          style={{
+            width: "180px",
+            display: "inline-block",
+            textAlign: "center",
+          }}
+        >
+          <p>Location: {weather.name}</p>
+          <p>Weather: {weather.weather[0].description}</p>
+          <p>Temperature: {weather.main.temp}</p>
+          <p>Wind Speed: {weather.wind.speed}</p>
+        </div>
+        <div
+          style={{
+            width: "180px",
+            display: "inline-block",
+            textAlign: "center",
+          }}
+        >
+          {this.renderWeatherIcon()}
+        </div>
       </div>
     ) : (
       <div>Loading..</div>
